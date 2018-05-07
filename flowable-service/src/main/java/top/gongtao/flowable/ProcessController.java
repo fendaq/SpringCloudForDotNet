b220844059f59f9d690ad91a5cd1f170a7cd27bc
t308 <postmaster@gongtao.top>
2018-05-08 07:29:02 +0800

    解决requestpayload接受参数问题

--- flowable-service/src/main/java/top/gongtao/flowable/ProcessController.java
+++ flowable-service/src/main/java/top/gongtao/flowable/ProcessController.java
@@ -1,5 +1,6 @@
 package top.gongtao.flowable;
 
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.flowable.engine.RepositoryService;
 import org.flowable.engine.RuntimeService;
 import org.flowable.engine.repository.ProcessDefinition;
@@ -7,10 +8,15 @@ import org.flowable.engine.runtime.ProcessInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -30,7 +36,7 @@ public class ProcessController {
 
 
     @PostMapping("/leave/form")
-    public String LeaveForm(@RequestParam(value = "date[]") String[] date,@RequestParam("reason") String reason){
+    public String LeaveForm(@RequestBody ObjectNode payload) throws ParseException {
 
 
         // 获取版本最新的流程定义对象
@@ -39,10 +45,17 @@ public class ProcessController {
         // 定义map集合存放流程参数
         Map<String,Object> variables = new HashMap<String, Object>();
 
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS Z");//注意格式化的表达式
+
+        payload.get("date").get(0).asText().replace("Z", " UTC");
+
+        Date startTime = format.parse(payload.get("date").get(0).asText().replace("Z", " UTC"));
+        Date endTime = format.parse(payload.get("date").get(1).asText().replace("Z", " UTC"));
+
         // 放入日期数据
-        variables.put("startTime",date[0]);
-        variables.put("endTime",date[1]);
-        variables.put("reason",reason); // 请假理由
+        variables.put("starttime",startTime.toString());
+        variables.put("endtime",endTime.toString());
+        variables.put("reason",payload.get("reason").asText()); // 请假理由
         variables.put("employee","mahang");
 
         // 启动带有表单定义的流程实例
