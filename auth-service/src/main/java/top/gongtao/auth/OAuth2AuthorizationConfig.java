39a2b59002ce8e14332dafe3ab3c7315b907549e
t308 <postmaster@gongtao.top>
2018-05-12 13:23:29 +0800

    '完善oauthserver'

--- auth-service/src/main/java/top/gongtao/auth/OAuth2AuthorizationConfig.java
+++ auth-service/src/main/java/top/gongtao/auth/OAuth2AuthorizationConfig.java
@@ -4,13 +4,23 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.oauth2.authserver.AuthorizationServerProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configurers.GlobalAuthenticationConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.code.AuthorizationCodeServices;
+import org.springframework.security.oauth2.provider.code.JdbcAuthorizationCodeServices;
+import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 
+import javax.sql.DataSource;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -27,6 +37,31 @@ public class OAuth2AuthorizationConfig extends AuthorizationServerConfigurerAdap
     @Autowired
     AuthorizationServerProperties authorizationServerProperties;
 
+    @Autowired
+    private DataSource dataSource;
+
+    private BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
+
+    /**
+     * The OAuth2 tokens are defined in the datasource defined in the
+     * <code>auth-server.yml</code> file stored in the Spring Cloud config
+     * github repository.
+     *
+     * @return
+     */
+    @Bean
+    public JdbcTokenStore tokenStore() {
+        return new JdbcTokenStore(dataSource);
+    }
+
+    @Bean
+    protected AuthorizationCodeServices authorizationCodeServices() {
+        return new JdbcAuthorizationCodeServices(dataSource);
+    }
+
+
+
     /**
      * 这里配置客户端详情服务，在这里完成客户端信息的初始化，可以在这里把客户端信息写死，也可以从数据库中获取。
      * @param clients
@@ -34,13 +69,24 @@ public class OAuth2AuthorizationConfig extends AuthorizationServerConfigurerAdap
      */
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-        clients.inMemory()
-                .withClient("demo")
-                .secret("demo")
-                .authorizedGrantTypes("password","authorization_code","refresh_token","implicit")
-                .scopes("read","write")
-                .accessTokenValiditySeconds((int) TimeUnit.HOURS.toSeconds(1))
-                .autoApprove(true);
+//        clients.inMemory()
+//                .withClient("demo")
+//                .secret("demo")
+//                .authorizedGrantTypes("password","authorization_code","refresh_token","implicit")
+//                .scopes("read","write")
+//                .accessTokenValiditySeconds((int) TimeUnit.HOURS.toSeconds(1))
+//                .autoApprove(true);
+        //从数据库中获取客户端信息
+        clients.jdbc(dataSource)
+                .passwordEncoder(passwordEncoder)
+                .withClient("client")
+                .authorizedGrantTypes("authorization_code", "client_credentials",
+                        "refresh_token","password", "implicit")
+                .authorities("ROLE_CLIENT")
+                .resourceIds("apis")
+                .scopes("read")
+                .secret("secret")
+                .accessTokenValiditySeconds(300);
     }
 
     /**
@@ -50,7 +96,8 @@ public class OAuth2AuthorizationConfig extends AuthorizationServerConfigurerAdap
      */
     @Override
     public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
-        security.tokenKeyAccess(authorizationServerProperties.getTokenKeyAccess());
+//        security.tokenKeyAccess(authorizationServerProperties.getTokenKeyAccess());
+        security.passwordEncoder(passwordEncoder);
     }
 
     /**
@@ -60,7 +107,10 @@ public class OAuth2AuthorizationConfig extends AuthorizationServerConfigurerAdap
      */
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-        endpoints.authenticationManager(authenticationManager).accessTokenConverter(jwtAccessTokenConverter());
+        //endpoints.authenticationManager(authenticationManager).accessTokenConverter(jwtAccessTokenConverter());
+        endpoints.authorizationCodeServices(authorizationCodeServices())
+                .authenticationManager(authenticationManager).tokenStore(tokenStore())
+                .approvalStoreDisabled();
     }
 
 
@@ -68,5 +118,36 @@ public class OAuth2AuthorizationConfig extends AuthorizationServerConfigurerAdap
     @ConfigurationProperties("jwt")
     JwtAccessTokenConverter jwtAccessTokenConverter() {
         return new JwtAccessTokenConverter();
+    }
+
+
+
+    /**
+     * Configure the {@link AuthenticationManagerBuilder} with initial
+     * configuration to setup users.
+     *
+     * @author anilallewar
+     *
+     */
+    @Configuration
+    @Order(Ordered.LOWEST_PRECEDENCE - 20)
+    protected static class AuthenticationManagerConfiguration extends
+            GlobalAuthenticationConfigurerAdapter {
+
+        @Autowired
+        private DataSource dataSource;
+
+        /**
+         * Setup 2 users with different roles
+         */
+        @Override
+        public void init(AuthenticationManagerBuilder auth) throws Exception {
+            // @formatter:off
+            auth.jdbcAuthentication().dataSource(dataSource).withUser("dave")
+                    .password("secret").roles("USER");
+            auth.jdbcAuthentication().dataSource(dataSource).withUser("anil")
+                    .password("password").roles("ADMIN");
+            // @formatter:on
+        }
     }
 }
